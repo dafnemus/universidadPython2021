79f4ae193b583d6de43361409029d85c168954bf
dafnemus <dafnemus@gmail.com>
2021-04-29 14:15:48 -0300

    declaracion de variables y direccion de memoria

---
+++ Variables/index.py
@@ -0,0 +1,23 @@
+# Declarar variables en Python.
+
+mi_variable = 'Hola desde Python'
+
+print(mi_variable)
+
+print()
+# Asignar un nuevo valor a la variable anterior:
+
+mi_variable = 'Hola Mundo desde Python'
+print(mi_variable)
+
+# Operación Suma con variables:
+
+x = 10
+y = 8
+suma = x + y
+
+print(f'{x} + {y} = {suma}')
+
+# Dirección de memoria
+
+print(f'La variable x({x}) se encuentra en {id(x)}')
