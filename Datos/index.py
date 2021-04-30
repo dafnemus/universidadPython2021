ffec33141bc4d1c9018cd3e69a6029ef161afab1
dafnemus <dafnemus@gmail.com>
2021-04-30 14:59:54 -0300

    ejemplo de tipos de datos bool, int, float, str

---
+++ Datos/index.py
@@ -0,0 +1,20 @@
+# Tipos de datos
+
+# Variable de tipo int
+x: int = 10
+# Imprimir por pantalla
+print(x)
+# Tipo 
+print(x, type(x))
+
+# Variable tipo Str
+x: str = 'Hola'
+print(x, type(x))
+
+# Variable tipo Float
+x: float = 2.3
+print(x, type(x))
+
+# Variable Booleana
+x: bool = False
+print(x, type(x))
