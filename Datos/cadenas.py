ae9965940726fca11b03f197a53f5a3b74d8c842
dafnemus <dafnemus@gmail.com>
2021-05-04 11:58:10 -0300

    manejo de cadenas

---
+++ Datos/cadenas.py
@@ -0,0 +1,23 @@
+# Cadenas(String)
+
+mi_grupo_favorito = 'BTS'
+print(mi_grupo_favorito)
+
+# Concatenación 
+
+print('Mi grupo favorito es: ' + mi_grupo_favorito)
+
+comentario = 'hace historia'
+print(mi_grupo_favorito + ' ' + comentario)
+
+mi_grupo_favorito += ' la mejor banda de Kpop'
+
+print(mi_grupo_favorito)
+
+# concatenación vs suma:
+numero_1 = '2'
+numero_2 = '4'
+
+print('Concatenación: ', numero_1 + numero_2)
+
+print('Suma:', int(numero_1) + int(numero_2))
