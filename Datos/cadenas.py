dcae46e8fea0897718fd8fa9e1097cea7251a077
dafnemus <dafnemus@gmail.com>
2021-05-10 17:08:24 -0300

    manejo basico de la funcion input

--- Datos/cadenas.py
+++ Datos/cadenas.py
@@ -21,3 +21,13 @@ numero_2 = '4'
 print('Concatenación: ', numero_1 + numero_2)
 
 print('Suma:', int(numero_1) + int(numero_2))
+
+# Funcion INPUT para procesar la entrada del usuario.
+resultado = input('Escribe un mensaje: ')
+print(f'mensaje: {resultado}')
+
+# conversion de input a int.
+num_1 = int(input('Escriba un número: '))
+num_2 = int(input('Escriba un número: '))
+
+print(f'{num_1} + {num_2} = {num_1 + num_2}')
