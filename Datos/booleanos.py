29941f0e6f1c6d5fdc765a3e3071279094eba740
dafnemus <dafnemus@gmail.com>
2021-05-10 16:36:14 -0300

    concepto basico de los bool

---
+++ Datos/booleanos.py
@@ -0,0 +1,11 @@
+booleano = True
+print(booleano)
+
+# redefino la variable
+booleano = 4 > 7
+print(booleano)
+
+if booleano:
+    print('El resultado es verdadero')
+else:
+    print('El resultado es falso')
