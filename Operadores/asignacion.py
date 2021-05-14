884ba19d3f4168ba39aaddba9bf7efbc2e61e0a1
dafnemus <dafnemus@gmail.com>
2021-05-14 17:03:25 -0300

    operadores de asignacion y reasignacion

---
+++ Operadores/asignacion.py
@@ -0,0 +1,13 @@
+# Operdaores de Asignaciṕn:
+
+# operador igual para asignar variables
+variable = 10
+print(f'variable: {variable}')
+
+# reasignación
+variable = variable + 1  # 11 
+print(f'variable reasignada: {variable}')
+
+# incremento con reasignación(válido con cualquier operador arimético)
+variable += 1  # 12
+print(f'Variable reasignada incrementada: {variable}')
