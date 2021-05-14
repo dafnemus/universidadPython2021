dfd7e9fe0e8c252c2d45e7463f9d615f1504482f
dafnemus <dafnemus@gmail.com>
2021-05-14 17:44:41 -0300

    comparacion de variables 1ra y 2da

---
+++ Operadores/comparacion.py
@@ -0,0 +1,28 @@
+# Operadores de Comparación
+
+primer_varable = 4
+segunda_variable = 3
+
+# Comparación de igual/igual:
+resultado = primer_variable == segunda_variable
+print(f'v1={primer_variable} \n v2={segunda_variable} \n son ==? {resultado}')
+
+# Comparación desigual/igual:
+resultado_2 = primer_variable != segunda_variable
+print(f'v1={primer_variable} \n v2={segunda_variable} \n son !=? {resultado_2}')
+
+# Comparación mayor que:
+resultado_2 = primer_variable > segunda_variable
+print(f'v1={primer_variable} es > que v2={segunda_variable}? {resultado_2}')
+
+# Comparación mayor/igual que:
+resultado_2 = primer_variable >= segunda_variable
+print(f'v1={primer_variable} es >= que v2={segunda_variable}? {resultado_2}')
+
+# Comparación menor que:
+resultado_2 = primer_variable < segunda_variable
+print(f'v1={primer_variable} es < que v2={segunda_variable}? {resultado_2}')
+
+# Comparación menor/igual que:
+resultado_2 = primer_variable <= segunda_variable
+print(f'v1={primer_variable} es <= que v2={segunda_variable}? {resultado_2}')
