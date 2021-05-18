22f0567c763bc7b7e3d14c4ed2c6a0fef8813f91
dafnemus <dafnemus@gmail.com>
2021-05-18 17:45:03 -0300

    operador ternario

---
+++ Operadores/ternario.py
@@ -0,0 +1,13 @@
+# Operdaor ternario:
+
+
+condicion = True
+
+# forma tipica
+if condicion:
+    print('Verdadero')
+else:
+    print('Falso')
+
+# version simplificada = ternario
+print('verdadero') if condicion else print('falso')
