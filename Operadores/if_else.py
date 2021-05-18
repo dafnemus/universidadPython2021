f70da14bd62618aba32720b7def395e3363d672e
dafnemus <dafnemus@gmail.com>
2021-05-18 16:34:45 -0300

    condicionales

---
+++ Operadores/if_else.py
@@ -0,0 +1,21 @@
+# Condicionales IF - ELSE
+
+condicion = True
+
+# mientras se reciba un valor distinto de vacio se devolvera TRUE
+
+# if/else basico
+if condicion:
+    print('Condicion es verdadera')
+else:
+    print('Condicion falsa')
+
+# if/elif/else
+palabra = False
+
+if palabra == True:
+    print('Condición verdadera')
+elif palabra == False:
+    print('Condicion Falsa')
+else:
+    print('Condición no reconocida')
