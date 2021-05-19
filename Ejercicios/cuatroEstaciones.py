36a0f3bf1a7ebbc4267d540b9f511069270cea39
dafnemus <dafnemus@gmail.com>
2021-05-19 16:36:18 -0300

    ejercicio estacion del mes resuelto

---
+++ Ejercicios/cuatroEstaciones.py
@@ -0,0 +1,31 @@
+# Ejercicio Estaciones del Año:
+#   -PRIMAVERA, VERANO, OTONO, INVIERNO.
+# Proporcionar un mes y determinar su estacion correspondiente.
+
+meses = ['enero', 'febrero','marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+
+estacion = None
+
+
+def determinar_estacion_año(mes):
+    for m in range(len(meses)):
+        if meses[m] == mes:
+            if m <= 1 or m == 11:
+                estacion = 'VERANO'
+                print(f'Es {estacion} en el mes de {mes}')
+            elif m == 2 or m <= 4:
+                estacion = 'OTOÑO'
+                print(f'Es {estacion} en el mes de {mes}')
+            elif m == 5 or m <= 7:
+                estacion = 'INVIERNO'
+                print(f'Es {estacion} en el mes de {mes}')
+            else:
+                estacion = 'PRIMAVERA'
+                print(f'Es {estacion} en el mes de {mes}')
+
+
+
+determinar_estacion_año('enero')
+determinar_estacion_año('julio')
+determinar_estacion_año('mayo')
+determinar_estacion_año('octubre')
