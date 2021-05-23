be0795a8036475cd241cbff06d4aff35c978100a
dafnemus <dafnemus@gmail.com>
2021-05-23 16:19:43 -0300

    sumando valores con funciones

--- Funciones/funciones_1.py
+++ Funciones/funciones_1.py
@@ -1,6 +1,33 @@
 # Funciones
 
-def mi_funcion():
+
+def mi_funcion() -> str:
     print('hola')
 
+
 mi_funcion()
+
+# argumentos y parametros
+def saludar(nombre: str, apellido: str) -> str:
+    print(f'Hola {nombre} {apellido}')
+
+
+saludar('Lujan', 'Sproviero')
+
+# uso del return
+def sumar(a: int, b: int) -> int:
+    return a + b
+
+
+resultado = sumar(2, 7)
+print(f'resultado de sumar: {resultado}')
+
+# parametros por defecto
+def sumar_v_2(a=0, b=0) -> int:
+    return a + b
+
+
+resultado = sumar_v_2()
+print(f'resultado de sumar: {resultado}')
+resultado = sumar_v_2(2, 8)
+print(f'resultado de sumar: {resultado}')
