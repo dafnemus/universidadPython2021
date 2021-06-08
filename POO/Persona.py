bedf83f2b6c762389ecec2d140d4242df24acc46
dafnemus <dafnemus@gmail.com>
2021-06-08 15:44:08 -0300

    manejo de la clase persona

--- POO/Persona.py
+++ POO/Persona.py
@@ -4,9 +4,19 @@ class Persona:
         self.apellido = apellido
         self.edad = edad
 
+    def mostrar_detalle(self):
+        return f'Persona: {self.nombre} {self.apellido}'
 
 persona_1 = Persona('Pablo', 'Perez', 22)
 print(f'Persona 1 se llama {persona_1.nombre}')
 
 persona_2 = Persona('Marta', 'Gomez', 45)
 print(f'persona 2, se llama {persona_2}')
+
+# Modificar valores
+persona_2.nombre = 'Marcela'
+print(f'persona 2, se llama {persona_2}')
+
+
+persona_1.mostrar_detalle()
+persona_2.mostrar_detalle()
