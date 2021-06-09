f22335a5ba0da5fb41999d274bcc290b599c148f
dafnemus <dafnemus@gmail.com>
2021-06-09 16:57:30 -0300

    practica de herencia multiple, creo la clase padre figurageometrica

--- POO/HerenciaMultiple/FiguraGeometrica.py
+++ POO/HerenciaMultiple/FiguraGeometrica.py
@@ -1,5 +1,6 @@
 # FiguraGeometrica
 
 class FiguraGeometrica:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, alto, ancho) -> None:
+        self.alto = alto
+        self.ancho = ancho
