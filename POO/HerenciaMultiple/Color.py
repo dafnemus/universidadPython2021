c15e657c1cbbf131cd45efcff829ac77642a8df9
dafnemus <dafnemus@gmail.com>
2021-06-12 16:51:05 -0300

    encapsulo el atributo color y agrego el metodo str

--- POO/HerenciaMultiple/Color.py
+++ POO/HerenciaMultiple/Color.py
@@ -1,3 +1,14 @@
 class Color:
     def __init__(self, color) -> None:
-        self.color = color
+        self._color = color
+
+    @property
+    def color(self):
+        return self._color
+    
+    @color.setter
+    def color(self, color):
+        self._color = color
+    
+    def __str__(self) -> str:
+        return f'Color: {self._color}'
