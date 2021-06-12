caf23e7d8e4e7cbcfb2ece24b221522621a73311
dafnemus <dafnemus@gmail.com>
2021-06-12 17:03:35 -0300

    realizo pruebas para la clase rectangulo

--- POO/HerenciaMultiple/test_figura_geometrica.py
+++ POO/HerenciaMultiple/test_figura_geometrica.py
@@ -1,12 +1,22 @@
-from Cuadrado import Cuadrado
+from POO.HerenciaMultiple.Rectangulo import Rectangulo
+from POO.HerenciaMultiple.Cuadrado import Cuadrado
 
 
 cuadrado_1 = Cuadrado(10, 'verde')
 print(f'El area del cuadrado 1 es: {cuadrado_1.calcular_area()}')
+print(cuadrado_1)
+print()
 
 cuadrado_2 = Cuadrado(3, 'rojo')
 print(f'El area del cuadrado 2 es: {cuadrado_2.calcular_area()}')
+print(cuadrado_2)
+print()
 
 # MRO - Method Resolution Order 
 # Permite conocer la jerarquía de clases
 print(f'Jerarquia de las clases dentro de la clase Cuadrado: {Cuadrado.mro()}')
+print()
+
+rectangulo_1 = Rectangulo(3, 4, 'violeta')
+print(rectangulo_1)
+print(f'Area de un rectangulo: {rectangulo_1.calcular_area()}')
