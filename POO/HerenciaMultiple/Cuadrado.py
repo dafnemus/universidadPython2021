fd19a5e8d857189936c0a380ccfdb5d336c2a00c
dafnemus <dafnemus@gmail.com>
2021-06-12 16:51:35 -0300

    agrego el metodo str

--- POO/HerenciaMultiple/Cuadrado.py
+++ POO/HerenciaMultiple/Cuadrado.py
@@ -7,8 +7,8 @@ class Cuadrado(FiguraGeometrica, Color):
         FiguraGeometrica.__init__(self, lado, lado)
         Color.__init__(self, color)
 
-    def __str__():
-        return f'Cuadrado {self.color}'
+    def __str__(self):
+        return f'{FiguraGeometrica.__str__(self)}, {Color.__str__(self)}'
 
     def calcular_area(self):
         return self.alto * self.ancho
