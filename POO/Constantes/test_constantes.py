9e56ca2b5c207a2273e5c2c52c6f92b72a6ef4a8
dafnemus <dafnemus@gmail.com>
2021-06-14 15:11:09 -0300

    manejo de constantes en clases

---
+++ POO/Constantes/test_constantes.py
@@ -0,0 +1,12 @@
+from POO.Constantes.constantes import CONSTANTE, Matematica
+
+# UNO PUEDE ACCEDER Y MODIFICARLAS
+print(CONSTANTE)
+
+# igualmente no debemos hacerlo
+# CONSTANTE = 'Nuevo valor'
+
+# print(CONSTANTE)
+
+# CONSTANTE EN CLASES
+print(Matematica.PI)
