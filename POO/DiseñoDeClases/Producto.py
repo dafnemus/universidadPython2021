14285a3b3ece995c8c574f638fa0b7a2dba574f2
dafnemus <dafnemus@gmail.com>
2021-06-14 17:03:19 -0300

    agrego u producto a la orden 2

--- POO/DiseñoDeClases/Producto.py
+++ POO/DiseñoDeClases/Producto.py
@@ -1,4 +1,5 @@
 class Producto:
+
     contador_preductos = 0
 
     def __init__(self, nombre: str, precio: float) -> None:
@@ -6,15 +7,15 @@ class Producto:
         self.id_producto = Producto.contador_preductos
         self.nombre = nombre
         self.precio = precio
-    
+
     def __str__(self) -> str:
         return f'''
             Id Producto {self.id_producto}:
             {self.nombre}______________${self.precio}
         '''
 
-# pruebas solo para esta clase
 
+# pruebas solo para esta clase
 if __name__ == '__main__':
     producto_1 = Producto('Remera', 1330.00)
     print(producto_1)
