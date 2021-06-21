fb3377e621c905e16172f92f9bab12f928674386
dafnemus <dafnemus@gmail.com>
2021-06-21 12:55:43 -0300

    manejo de excepciones, especificas, generalizadas

---
+++ Excepciones/excepciones.py
@@ -0,0 +1,36 @@
+# Uso de try/except para manejar excepciones.
+
+resultado = None
+
+
+# Captura generica de un error 
+try:
+    a = '10'
+    b = 0
+    resultado = a / b
+except Exception as e:
+    print(f'Ocurrió el siguiente error: {e}')
+
+print(f'Resultado: {resultado}')
+print('Continuamos...')
+
+
+# De excepciones especificas a genericas
+try:
+    print('DIVISION, ingrese dos numeros')
+    a = int(input('Primer número: '))
+    b = int(input('Segundo número: '))
+    resultado = a / b
+except ZeroDivisionError as zde:
+    print(f'ZDE-Ocurrió el siguiente error: {zde}')
+except TypeError as te:
+    print(f'TE-Ocurrió el siguiente error: {te}')
+except Exception as e:
+    print(f'E-Ocurrió el siguiente error: {e}')
+else:  # solo si no hubo ningún error
+    print('EJECUCIÓN EXITOSA')
+finally:  # se ejecutará siempre
+    print('Ejecución finalizada')
+
+print(f'Resultado: {resultado}')
+print('Continuamos...')
