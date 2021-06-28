2feafc304d69cb81dcbe1a89251f08f491f27d20
dafnemus <dafnemus@gmail.com>
2021-06-28 10:01:46 -0300

    conexion de python con postgres

---
+++ BD/prueba.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+# conexion a la base de datos, los valores son a modo de ejemplo.
+conexion = psycopg2.connect(user='usuario', password='contraseña', host='localhost', port='5432',
+database='base de datos')
+
+print(conexion)
+
+# creacion de un cursor
+cursor = conexion.cursor()
+
+sentencia = 'SELECT * FROM persona'
+cursor.execute(sentencia)
+# traer los registros de la db.
+registros = cursor.fetchall()
+print(registros)
+
+# cerrar la conecion a la db
+print(registros)
