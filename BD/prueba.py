ffefb4183ac67eb102192f71feb9130d7990c609
dafnemus <dafnemus@gmail.com>
2021-07-09 17:21:43 -0300

    recuperar mas de un registro con fetchall

--- BD/prueba.py
+++ BD/prueba.py
@@ -26,14 +26,13 @@ conexion.close()
 
 # %s = placeholder o parametro posicional
 
+# obtener un registro
 try:
     with conexion:
         with conexion.cursor() as cursor:
             sentencia = 'SELECT * FROM persona WHERE id_persona = %s'
             id_persona = input('Proporcione el id_persona que quiere recuperar: ')
             cursor.execute(sentencia, (id_persona,))
-            # traer los registros de la db.
-            # registros = cursor.fetchall()
             # recuperar un solo registro
             registros = cursor.fetchone()
             print(registros)
@@ -42,3 +41,26 @@ except Exception as e:
 finally:
     conexion.close()
 
+
+conexion_2 = psycopg2.connect(user='dafne', password='dafne', host='localhost', port='5432',
+database='dafne')
+
+# obtener varios registros:
+# IN= in dica en varios registros
+try:
+    with conexion_2:
+        with conexion_2.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona WHERE id_persona IN %s'
+            # llaves = ((1,2),)
+            entrada = input('Proporciones los id a recuperar separados por coma: ')
+            llaves = (tuple(entrada.split(',')),)
+            cursor.execute(sentencia, llaves)
+            # traer los registros de la db.
+            # registros = cursor.fetchall()
+            registros = cursor.fetchall()
+            for registro in registros:
+                print(registro)
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    conexion_2.close()
