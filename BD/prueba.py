d6513dbb05cc245f42d35e577886ddbbfcf6bdb8
dafnemus <dafnemus@gmail.com>
2021-07-09 17:00:56 -0300

    recuperar un registro de una base de datos

--- BD/prueba.py
+++ BD/prueba.py
@@ -1,8 +1,8 @@
 import psycopg2
 
 # conexion a la base de datos, los valores son a modo de ejemplo.
-conexion = psycopg2.connect(user='usuario', password='contraseña', host='localhost', port='5432',
-database='base de datos')
+conexion = psycopg2.connect(user='dafne', password='dafne', host='localhost', port='5432',
+database='dafne')
 
 print(conexion)
 
@@ -15,13 +15,27 @@ cursor.close()
 conexion.close()
 '''
 # conexion con with
+# base de datos
+# sentencia = 'SELECT * FROM persona'
+# recuperar columnas
+# sentencia = 'SELECT id_persona, nombre FROM persona' 
+# recuperar el registro de un id en particular
+# sentancia = 'SELECT * FROM persona WHERE id_persona = 1'
+# recuperar el registro de un id en particular a través de un parametro
+# sentencia = 'SELECT * FROM persona WHERE id_persona = %s'
+
+# %s = placeholder o parametro posicional
+
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            sentencia = 'SELECT * FROM persona'
-            cursor.execute(sentencia)
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s'
+            id_persona = input('Proporcione el id_persona que quiere recuperar: ')
+            cursor.execute(sentencia, (id_persona,))
             # traer los registros de la db.
-            registros = cursor.fetchall()
+            # registros = cursor.fetchall()
+            # recuperar un solo registro
+            registros = cursor.fetchone()
             print(registros)
 except Exception as e:
     print(f'Ocurrió un error: {e}')
