622dd029de8ea8b52a5bc02ab9a59796486fb208
dafnemus <dafnemus@gmail.com>
2021-07-09 18:25:06 -0300

    actualizacion del registro 6 y 7 de la db

--- BD/actualizar.py
+++ BD/actualizar.py
@@ -17,3 +17,24 @@ except Exception as e:
     print(f'Ocurrió un error: {e}')
 finally:
     conexion_2.close()
+
+
+conexion_3 = psycopg2.connect(user='dafne', password='dafne', host='localhost', port='5432',
+database='dafne')
+
+
+try:
+    with conexion_3:
+        with conexion_3.cursor() as cursor:
+            sentencia = 'Update persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+            valores = (
+                ('Juan', 'Martinez', 'jmartinez@gmail.com', 6),
+                ('Marta', 'Sanchez', 'msanchez@gmail.com', 7)
+            )
+            cursor.executemany(sentencia, valores)
+            registros_actualizados = cursor.rowcount
+            print(f'Registros actualizados: {registros_actualizados}')
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    conexion_3.close()
